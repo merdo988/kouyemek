3ac47be7ece97f14784664c297ab126dcb913aca
merdo988 <mertinal1998@hotmail.com>
2019-09-28 13:35:17 +0300

    Splash Screen Desing

---
+++ app/src/main/java/net/kodlar/kouyemek/SplashActivity.java
@@ -0,0 +1,16 @@
+package net.kodlar.kouyemek;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
