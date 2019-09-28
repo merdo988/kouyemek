e0e74e9e5cdeca0aa103c80ee416684644c7cfeb
emre-sualp <160201103@kocaeli.edu.tr>
2019-09-28 18:25:15 +0300

    test splash

--- app/src/main/java/net/kodlar/kouyemek/SplashActivity.java
+++ app/src/main/java/net/kodlar/kouyemek/SplashActivity.java
@@ -11,8 +11,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(SplashActivity.this,MainActivity.class);
         startActivity(intent);
-        System.out.println("vvvvv");
-        System.out.println("llll");
+        System.out.println("test123");
         finish();
     }
 }
